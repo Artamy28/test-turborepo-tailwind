bdc12ed8915cc75bd9b1b703bf29357d7f3481bb
Artamy28 <ung.vrj@gmail.com>
2024-03-01 10:43:13 +0000

    Fix issue with tailwind, add NextUI, fix issue with NextUI

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -3,14 +3,32 @@ import { Button } from "@nextui-org/button";
 export default function Page(): JSX.Element {
   return (
     <>
-			<div className="flex flex-col gap-4 p-4 min-h-screen items-center justify-center">
-				<button
-					type="button"
-					className="rounded-xl bg-blue-500 py-2 px-2.5 text-white hover:bg-blue-600"
-				>
-					Tailwind Button!
-				</button>
-			</div>
-		</>
+		<div className="flex flex-col gap-4 p-4 min-h-screen items-center justify-center">
+			<Button color="primary" variant='bordered'>
+				Default
+			</Button>
+			<Button color="primary">
+				Primary
+			</Button>
+			<Button color="secondary">
+				Secondary
+			</Button>
+			<Button color="success">
+				Success
+			</Button>
+			<Button color="warning">
+				Warning
+			</Button>
+			<Button color="danger">
+				Danger
+			</Button>
+			<button
+				type="button"
+				className="rounded-xl bg-blue-500 py-2 px-2.5 text-white hover:bg-blue-600"
+			>
+				Tailwind Button!
+			</button>
+		</div>
+	</>
   );
 }
--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -3,13 +3,27 @@
 import type { Config } from "tailwindcss";
 import sharedConfig from "@repo/tailwind-config";
 
+import { nextui } from "@nextui-org/react";
+
 /** @type {import('tailwindcss').Config} */
-const config: Pick<Config, "content" | "presets"> = {
+const config: Pick<Config, "content" | "presets" | "darkMode" | "plugins"> = {
   content: [
-    "./src/app/**/*.tsx",
-    "../../packages/ui/*.{js,ts,jsx,tsx}"
+    // "./src/app/**/*.tsx",
+    // "../../packages/ui/*.{js,ts,jsx,tsx}",
+    // "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+    // "./**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/app/*.{js,ts,jsx,tsx,mdx}",
+		// "./app/**/*.{js,ts,jsx,tsx,mdx}",
+		// "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+		// "./components/**/*.{js,ts,jsx,tsx,mdx}",
+		// "./providers/**/*.{js,ts,jsx,tsx,mdx}",
+		"../../packages/ui/**/*.{js,ts,jsx,tsx,mdx}",
+		"../.././node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+		"./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   presets: [sharedConfig],
+  darkMode: "class",
+  plugins: [nextui()]
 };
 
 export default config;
